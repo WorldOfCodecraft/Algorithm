07dd085d7fb3c1d714a3ef4de70f689e4769e4ec
T <41558465+WorldOfCodecraft@users.noreply.github.com>
2020-02-26 09:13:13 -0500

    Added bubble sort immplements

---
+++ src/algorithms/sort/S01_Bubble.java
@@ -0,0 +1,28 @@
+package algorithms.sort;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class S01_Bubble {
+    public static void main(String[] args) {
+        int[] from = {20, 35, -15, 7, 55, 1, -22};
+        for (int i = 0; i < from.length; i++) {
+            int unsortedIndex = from.length - 1;
+            for (int j = 0; j < unsortedIndex; j++) {
+                if (from[j] > from[j + 1]) {
+                    swap(from, j, j + 1);
+                }
+            }
+            Arrays.stream(from).boxed().collect(Collectors.toList()).forEach(item -> System.out.print(item + " "));
+            System.out.println();
+        }
+
+    }
+
+    public static void swap(int[] from, int i, int j) {
+        int temp = from[i];
+        from[i] = from[j];
+        from[j] = temp;
+    }
+}
