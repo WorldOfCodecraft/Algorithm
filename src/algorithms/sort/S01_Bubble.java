bdfc09e40258472e80668fd85a6fadebadd85dba
T <41558465+WorldOfCodecraft@users.noreply.github.com>
2020-02-29 09:19:13 -0500

    Added bubble sort immplements improved

--- src/algorithms/sort/S01_Bubble.java
+++ src/algorithms/sort/S01_Bubble.java
@@ -7,8 +7,19 @@ import java.util.stream.Stream;
 public class S01_Bubble {
     public static void main(String[] args) {
         int[] from = {20, 35, -15, 7, 55, 1, -22};
-        for (int i = 0; i < from.length; i++) {
-            int unsortedIndex = from.length - 1;
+//        for (int i = 0; i < from.length; i++) {
+//            int unsortedIndex = from.length - 1;
+//            for (int j = 0; j < unsortedIndex; j++) {
+//                if (from[j] > from[j + 1]) {
+//                    swap(from, j, j + 1);
+//                }
+//            }
+//            Arrays.stream(from).boxed().collect(Collectors.toList()).forEach(item -> System.out.print(item + " "));
+//            System.out.println();
+//        }
+
+        //Improved version, more intuitive
+        for (int unsortedIndex = from.length - 1; unsortedIndex > 0; unsortedIndex--) {
             for (int j = 0; j < unsortedIndex; j++) {
                 if (from[j] > from[j + 1]) {
                     swap(from, j, j + 1);
@@ -17,7 +28,6 @@ public class S01_Bubble {
             Arrays.stream(from).boxed().collect(Collectors.toList()).forEach(item -> System.out.print(item + " "));
             System.out.println();
         }
-
     }
 
     public static void swap(int[] from, int i, int j) {
