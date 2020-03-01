e26eb98351bc84537c98e619e7c7ae9d94f6649f
T <41558465+WorldOfCodecraft@users.noreply.github.com>
2020-03-01 09:30:39 -0500

    Added bubble sort implements algorithms description

--- src/algorithms/sort/S01_Bubble.java
+++ src/algorithms/sort/S01_Bubble.java
@@ -7,6 +7,11 @@ import java.util.stream.Stream;
 public class S01_Bubble {
     public static void main(String[] args) {
         int[] from = {20, 35, -15, 7, 55, 1, -22};
+        /**
+         * 1. We compare one ele to the next
+         * 2. If the one is greater, we swap them
+         * 3. After one loop, we do step 1 and 2 again, but not to the last sorted element
+         */
 //        for (int i = 0; i < from.length; i++) {
 //            int unsortedIndex = from.length - 1;
 //            for (int j = 0; j < unsortedIndex; j++) {
