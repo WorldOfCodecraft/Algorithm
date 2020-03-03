1745f9649d3b0ad0e3c52d979f27d1ca7479fe10
T <41558465+WorldOfCodecraft@users.noreply.github.com>
2020-03-03 15:30:22 -0500

    Changed the base package name to better sort the sub-packages

--- src/algorithms/a_sort/S01_Bubble.java
+++ src/algorithms/a_sort/S01_Bubble.java
@@ -1,8 +1,7 @@
-package algorithms.sort;
+package algorithms.a_sort;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class S01_Bubble {
     public static void main(String[] args) {
--- src/algorithms/a_sort/S02_Selection.java
+++ src/algorithms/a_sort/S02_Selection.java
@@ -1,4 +1,4 @@
-package algorithms.sort;
+package algorithms.a_sort;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
